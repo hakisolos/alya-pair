393f6c99eef3fc83ae29d1e70b2d2ed839118d58
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-11-16 16:09:04 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  PREFIX: process.env.PREFIX || "ALYA-V2x",
+  EMAIL: process.env.EMAIL || "maxwellexcel2@gmail.com",
+  PASS: process.env.PASS || "mynameisexcel2",
+  MESSAGE: process.env.MESSAGE || "ALYA PAIR CODE CONNECTED✅️",
+  IMAGE: process.env.IMAGE || "https://itzpire.com/file/43bc3e53a19b.jpg",
+}
